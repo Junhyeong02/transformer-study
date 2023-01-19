6efe20562ac1b48f1678a60f9b9deaf83b40d899
KONG Junhyeong <denovokjh@snu.ac.kr>
2023-01-19 11:54:25 +0900

    implement get_dataset

--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
 import torch
+from torch.utils.data import random_split
 
 def get_attn_decoder_mask(tensor):
     mask = torch.ones_like(tensor, requires_grad = False)
     mask = torch.tril(mask)
 
     return mask
+
+def get_dataset(dataset, split_ratio):
+
+    len_train_dataset = int(len(dataset)/ split_ratio)
+    len_test_dataset = len(dataset) - len_train_dataset
+    
+    train_dataset, test_dataset =  random_split(dataset, [len_train_dataset, len_test_dataset])
+
+    return train_dataset, test_dataset
 
 if __name__ == "__main__":
     size = 20
